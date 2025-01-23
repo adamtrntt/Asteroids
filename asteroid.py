d0b32bbda26ec3c04b62274acf3de11b2d295e1f
adamtrnt <adamtrnt1@gmail.com>
2025-01-23 16:14:00 +1100

    asteroids

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+import pygame
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 255, 255), self.position, self.radius, 2)
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 from constants import *
+from player import *
+from asteroid import *
+from AsteroidField import *
 
 def main():
     import pygame
     pygame.init()
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+    Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroid_group, updatable_group, drawable_group)
+    AsteroidField.containers = (updatable_group,)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
+
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        dt = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        updatable_group.update(dt)
+        for sprite in drawable_group:
+            sprite.draw(screen)
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
